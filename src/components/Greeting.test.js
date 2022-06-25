6ae4da89a806c4eb6e1c648fe2cddfa39ec52834
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-25 12:48:18 +0300

    add testing suite

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,17 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('renders Hello World as a text', () => {
-  // Arrange - set up test data, conditions and environment
-  render(<Greeting />);
+describe('Greeting component', () => {
+  test('renders Hello World as a text', () => {
+    // Arrange - set up test data, conditions and environment
+    render(<Greeting />);
 
-  // Act - run the logic that should be tested
+    // Act - run the logic that should be tested
 
-  // Assert - compare execution result with expected result
+    // Assert - compare execution result with expected result
 
-  // get -> returns an error if element not found
-  // query -> won't return an error
-  // find -> returns a promise
-  const helloWorldElement = screen.getByText('Hello World!');
-  expect(helloWorldElement).toBeInTheDocument();
+    // get -> returns an error if element not found
+    // query -> won't return an error
+    // find -> returns a promise
+    const helloWorldElement = screen.getByText('Hello World!');
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
